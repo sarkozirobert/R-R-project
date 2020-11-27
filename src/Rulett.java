e0023a951091bf36c2e0b98e933d28646b342c62
Ria <benedek.maria88@gmail.com>
2020-11-27 13:27:51 +0100

    Rulett is back again yay!

---
+++ src/Rulett.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class Rulett {
+
+
+    private Map<String, ArrayList<Integer>> fields;
+    private int winnerNum;
+    private String winnerNumColor;
+
+    public Rulett() {
+        Integer[] red = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+        Integer[] black = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+
+        ArrayList<Integer> reds = new ArrayList<>(Arrays.asList(red));
+        ArrayList<Integer> blacks = new ArrayList<>(Arrays.asList(black));
+        ArrayList<Integer> theNull = new ArrayList<>();
+        theNull.add(0);
+
+        fields.put("red", reds);
+        fields.put("black", blacks);
+        fields.put("green", theNull);
+    }
+
+    public int getWinnerNum() {
+        return winnerNum;
+    }
+
+    public String getWinnerNumColor() {
+        return winnerNumColor;
+    }
+
+    public void spin() {
+        winnerNum = (int)(Math.random() * 37);
+
+        for (Map.Entry<String, ArrayList<Integer>> entry : fields.entrySet()) {
+            if (entry.getValue().contains(winnerNum)) {
+                winnerNumColor = entry.getKey();
+                break;
+            }
+        }
+    }
+    
+}
