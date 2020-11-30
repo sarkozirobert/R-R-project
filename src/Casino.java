805374ff792ec72d7c18e6d77f63d0338cdea1b5
Ria <benedek.maria88@gmail.com>
2020-11-30 12:51:53 +0100

    countWinPrice method

--- src/Casino.java
+++ src/Casino.java
@@ -44,7 +44,7 @@ public class Casino {
         for (Player p : players) {
             p.setBalance(-p.getActualBetSize());
             if (p.getNumbersToBetOn().contains(winnerNum)) {
-                int winPrice = countWinnerPrice(p);
+                int winPrice = countWinPrice(p);
                 p.setProfitNloss(winPrice);
                 p.setBalance(winPrice);
             } else {
@@ -53,10 +53,31 @@ public class Casino {
         }
     }
 
-    public int countWinnerPrice(Player p) {
-        // megkapja a győztes számot, illetve a játékos által feltett összeget
+    public int countWinPrice(Player p) {
+        // megkapja a játékos által feltett összeget
         // ebből számolja ki, hogy mennyit kap a játékos
-        // itt szerintem érdemes HashMap-be berakni a különböző nyerőosztályokat
+
+        switch (p.getNumbersToBetOn().size()) {
+            // ha egy számra rakott a játékos
+            case 1:
+                return p.getActualBetSize() * 36;
+            // ha két számra rakott
+            case 2:
+                return p.getActualBetSize() * 18;
+            // ha négy számra rakott
+            case 4:
+                return p.getActualBetSize() * 8;
+            // ha hat számra rakott
+            case 6:
+                return p.getActualBetSize() * 6;
+            // ha "tucatra" rakott
+            case 12:
+                return p.getActualBetSize() * 4;
+            // ha színre, páros-páratlanra vagy 1-18/19-36-ra rakott
+            case 18:
+                return p.getActualBetSize() * 2;
+
+        }
 
         return 0;
     }
