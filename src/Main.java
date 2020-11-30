3dd6dae6d818d63a58d849a4db2fe53b7eb01cd0
Ria <benedek.maria88@gmail.com>
2020-11-30 13:41:43 +0100

    started writing main method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Main m = new Main();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mit szeretne csinálni? Adja meg a számot!");
+        System.out.println("1 - szimuláció");
+        System.out.println("2 - játék");
+
+        String answer = input.nextLine();
+
+        switch (answer) {
+            case "1":
+                m.simulation();
+                break;
+            case "2":
+                m.userPlay(input);
+                break;
+            default:
+                System.out.println("Eee, rossz válasz! Program vége.");
+        }
+
+        input.close();
+
     }
+
+    public void simulation() {
+        // things happen here
+    }
+
+    public void userPlay(Scanner input) {
+        // other things happen here
+    }
+
 }
