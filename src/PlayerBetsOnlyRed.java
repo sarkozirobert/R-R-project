04d03ef40841646664c345822a96331019260554
Sárközi Róbert <sark.robert@gmail.com>
2020-12-01 13:27:38 +0100

    waste deleted

--- src/PlayerBetsOnlyRed.java
+++ src/PlayerBetsOnlyRed.java
@@ -8,21 +8,6 @@ public class PlayerBetsOnlyRed extends Player{
 
     @Override
     public void play() {
-        //Main-be még jó lehet!
-
-        /*Scanner sc1 = new Scanner(System.in);
-        System.out.println("Adja meg a számokat, amikre tétet szeretne tenni! (Szóközzel elválasztva adja meg a számokat)");
-        String line = sc1.nextLine();
-        String[] parts = line.split(" ");
-
-
-        for (int i = 0; i < parts.length; i++) {
-            int actualNr = Integer.parseInt(parts[i]);
-            this.getNumbersToBetOn().add(actualNr);
-        }
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("Adja meg a tét összegét! (Számot írjon be!)");
-        this.setActualBetSize(sc2.nextInt());*/
 
         ArrayList<Integer> betNums = getCasino().getRulettWheel().getFields().get("red");
         setNumbersToBetOn(betNums);
